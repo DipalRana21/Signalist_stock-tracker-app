d8252058a41aaa230b4782f220d0a535ab76a765
Dipal Rana <rdipal993@gmail.com>
2025-12-10 11:49:51 +0530

    Ignre eslint and typescript during builds

--- app/(root)/stocks/[symbol]/page.tsx
+++ app/(root)/stocks/[symbol]/page.tsx
@@ -1,5 +1,6 @@
 import TradingViewWidget from "@/components/TradingViewWidget";
 import WatchlistButton from "@/components/WatchlistButton";
+
 import {
   SYMBOL_INFO_WIDGET_CONFIG,
   CANDLE_CHART_WIDGET_CONFIG,
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  // @ts-ignore: 'eslint' type definition might be missing in older types
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
